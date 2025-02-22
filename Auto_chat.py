9e865a0d1583d3f8d244869ba391233ed913140d
Abu-saiba <abusaiba9956@gmail.com>
2025-02-22 21:00:18 +0530

    changing api for security reason

--- Auto_chat.py
+++ Auto_chat.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 # pip install openai 
 # if you saved the key under a different environment variable name, you can do something like:
 client = OpenAI(
-  api_key="k-proj-poMml30SjvEXTFtPogCvk830xHQ_JU8_MY7AvbHW7HJB2JnhOsMOSd3HHrPZG0u5UZx3oX7nLwT3BlbkFJ6QF6gsSEmzOppNw7RKevtgbDaDcesk3njR67pSXRuZYscZdmHsTGhs7CdU-wLmko8Kwg8LALkA" #MY actual OPEnAI key API
+  api_key="hhjlk;jhjjkl;l" #MY actual OPEnAI key API
 )
 
 command = '''
